82163b7c4c02af3454cfc4dcee79503c0862e382
Alsaahir <jacobwilsonpmwale@gmail.com>
2022-10-02 20:06:18 +0200

    first push

---
+++ FOAM_MASTERS/fm_website/main_site/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+# from django.views.generic import TemplateView
+
+
+urlpatterns = [
+   path('about/', views.about, name='about'),
+   path('blog_details/', views.blog_details, name='blog_details'),
+   path('blog_home/', views.blog_home, name='blog_home'),
+   path('contact_us/', views.contact_us, name='contact_us'),
+   path('elements/', views.elements, name='elements'),
+   path('', views.index, name='index'),
+   path('menu/', views.menu, name='menu'),
+   path('tests/', views.menu, name='test'),
+]
---
+++ FOAM_MASTERS/fm_website/main_site/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
+def blog_details(request):
+    return render(request, 'blog-details.html', {})
+
+def blog_home(request):
+    return render(request, 'blog-home.html', {})
+
+def contact_us(request):
+    return render(request, 'contact-us.html', {})
+
+def elements(request):
+    return render(request, 'elements.html', {})
+
+def index(request):
+    return render(request, 'index.html', {})
+
+def menu(request):
+    return render(request, 'menu.html', {})
+
+def test(request):
+    return render(request, 'test.html', {})
