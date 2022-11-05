037fdfcc998ede74c613406ef3f668158acf59b4
Alsaahir <jacobwilsonpmwale@gmail.com>
2022-11-05 15:10:18 +0200

    store created

--- FOAM_MASTERS/fm_website/fm_website/settings.py
+++ FOAM_MASTERS/fm_website/fm_website/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main_site.apps.MainSiteConfig',
     'users.apps.UsersConfig',
+    'store.apps.StoreConfig',
 ]
 
 MIDDLEWARE = [
--- FOAM_MASTERS/fm_website/fm_website/urls.py
+++ FOAM_MASTERS/fm_website/fm_website/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_site.urls')),
     path('', include('users.urls')),
+    path('', include('store.urls')),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
