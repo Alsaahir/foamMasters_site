493c0a6fb26c25d0bee7d8d11d611a4b3e0c48fb
Alsaahir <jacobwilsonpmwale@gmail.com>
2022-12-19 03:09:44 -0800

    pics now loading

--- FOAM_MASTERS/fm_website/fm_website/urls.py
+++ FOAM_MASTERS/fm_website/fm_website/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -25,3 +27,4 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
